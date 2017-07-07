b67a05ff7761f5aae276b484a013b466991ab562
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 16:47:05 +0900

    Problem07 finish

--- src/com/javaex/problem07/Account.java
+++ src/com/javaex/problem07/Account.java
@@ -6,7 +6,36 @@ public class Account {
     private int balance;
     
     //생성자 작성
+    public Account() {
+    	
+    }
+    
+    public Account(String accountNo) {
+    	this.accountNo = accountNo;
+    	this.balance = 0;
+    }
+    
+    
+    public Account(String accountNo, int balance) {
+    	this.accountNo = accountNo;
+    	this.balance = balance;
+    }
     
     //필요한 메소드 작성
+    public void deposit(int money) {
+    	balance += money;
+    }
+    
+    public void withdraw(int money) {
+    	if(money<=balance) {				//잔고보다 출금이 적을 경우 인출
+    		balance -= money;
+		} else {							//잔고보다 출금이 많을 경우 
+			System.out.println("잔고가 부족합니다.");
+			System.out.println("잔고액 : "+balance);
+		}
+    }
 
+    public void showBalance() {
+    	System.out.println(balance);
+    }
 }
