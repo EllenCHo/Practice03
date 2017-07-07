22136ed6c975338279e52b5f0eae9ceaecd17bb4
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 16:11:23 +0900

    Problem05 finish

--- src/com/javaex/problem05/StringUtil.java
+++ src/com/javaex/problem05/StringUtil.java
@@ -2,10 +2,16 @@ package com.javaex.problem05;
 
 public class StringUtil {
     
-    public static String concatString(){
-       
+    public static String concatString(String[] str){
+    	String result = "";
+    	
         //메소드 내용작성
+    	for(int i = 0; i<str.length; i++) {
+    		result += str[i];
+    	}
         
+    	
+    	return result;
     }
 
 }
