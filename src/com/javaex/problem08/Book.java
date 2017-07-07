7b8763dbc44bb5cc22e97995953b4739d26eeab0
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 17:09:10 +0900

    Problem08 finish

--- src/com/javaex/problem08/Book.java
+++ src/com/javaex/problem08/Book.java
@@ -1,7 +1,65 @@
 package com.javaex.problem08;
 
 public class Book {
+    private int bookNo;			
+    private String title;
+    private String author;
+    private int stateCode;
     
+    public Book(int bookNo, String title, String author) {
+    	this.bookNo = bookNo;
+    	this.title = title;
+    	this.author = author;
+    	stateCode = 1;
+    }
+    
+    public int getBookNo() {
+    	return bookNo;
+    }
+    
+    public String getTitle() {
+    	return title;
+    }
+    
+    public String getAuthor() {
+    	return author;
+    }
+    
+    public int getStateCode() {
+    	return stateCode;
+    }
+    
+    public void setBookNo(int bookNo) {
+    	this.bookNo = bookNo;
+    }
+    
+    public void setTitle(String title) {
+    	this.title = title;
+    }
+    
+    public void setAuthor(String author) {
+    	this.author = author;
+    }
+    
+    public void setStateCode(int stateCode) {
+    	this.stateCode = stateCode;
+    }
+    public void rent(int num) {
+    	if(bookNo == num) {
+    		stateCode = 0;
+    		System.out.println(title + "이(가) 대여 됐습니다.");
+    	}
+    	
+    }
+    
+    public void print() {
+    	if(stateCode==1) {
+    		System.out.println(bookNo + "\t" + "책 제목:" + title + "\t" + "작가:" + author + "\t대여 유무 : 재고 있음");
+    	} else {
+    		System.out.println(bookNo + "\t" + "책 제목:" + title + "\t" + "작가:" + author + "\t대여 유무 : 대여중");
+    	}
+    	
+    }
     
     
     
--- src/com/javaex/problem08/BookShop.java
+++ src/com/javaex/problem08/BookShop.java
@@ -27,7 +27,9 @@ public class BookShop {
         scanner.close();
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
-       
+        for(int i = 0; i<books.length; i++) {
+        	books[i].rent(num);
+        }
 
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
@@ -35,6 +37,8 @@ public class BookShop {
 
     //(2)전달받은 배열을 모두 출력하는 메소드
     private static void displayBookInfo(Book[] books) {
-        //코드작성
+        for(int i = 0; i<books.length; i++) {
+        	books[i].print();
+        }
     }
 }
